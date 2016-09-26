1fcb90dee148031c1dab50ec5bb75f0cc77a171e
MacNatch <MacNatch@172.29.103.170>
2016-09-26 10:15:39 -0400

    Test 2

--- src/helloworld/HelloWorld.java
+++ src/helloworld/HelloWorld.java
@@ -16,6 +16,7 @@ public class HelloWorld {
      */
     public static void main(String[] args) {
         System.out.println("Hello World");
+        System.out.println("Ici on va modifier le premier test");
     }
     
 }
